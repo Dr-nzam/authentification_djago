0734df0ebc5a248902219e71f8a3b7c8bacd574d
Dr-nzam <nzambouarmel@gmail.com>
2023-04-28 11:53:37 +0100

    login correct

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import register,index
+from .views import register,connexion,index
 
 
 urlpatterns = [
     path('register', register, name='register'),
+    path('login', connexion, name='login'),
     path('index', index , name='index')
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import redirect, render
 from django.contrib import messages
 from .form import UserForm
+from django.contrib.auth import authenticate, login
+
 
 def register(request):
     form = UserForm()
@@ -11,7 +13,18 @@ def register(request):
             messages.success(request, "votre compte a ete bien creer")
             return redirect(index)
     context = {'form':form}
-    return render(request, 'core/register.html',context) 
+    return render(request, 'core/register.html',context)
+
+def connexion(request):
+    if request.method == 'POST':
+        nameUser= request.POST['nameuser'] 
+        password= request.POST['pass'] 
+        user = authenticate(request, username=nameUser, password=password)
+        if user is not None and user.is_active:
+            login(request,user)
+            messages.success(request, 'welcome back')
+            return redirect(index)
+    return render(request, 'core/login.html')
 
 def index(request):
    return render(request, 'core/index.html')
