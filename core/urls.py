3bde2dab0b9e703ed2ca144cca97b39379785d7d
Dr-nzam <nzambouarmel@gmail.com>
2023-04-28 10:41:34 +0100

    enregistrement user

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import register,index
 
 
 urlpatterns = [
-
+    path('register', register, name='register'),
+    path('index', index , name='index')
 ]
--- core/views.py
+++ core/views.py
@@ -1,2 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from .form import UserForm
 
+def register(request):
+    form = UserForm()
+    if request.method =='POST':
+        form = UserForm(data = request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "votre compte a ete bien creer")
+            return redirect(index)
+    context = {'form':form}
+    return render(request, 'core/register.html',context) 
+
+def index(request):
+   return render(request, 'core/index.html')
